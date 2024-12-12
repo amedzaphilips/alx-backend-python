1527f5459cd7094dfc810e4e519a7ba8caabdc8c
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-12 12:43:35 +0000

    task 1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ annotated function that concats """
 
-def concat(string: str1, string: str2) -> str:
+def concat(str1: string, str2: string) -> str:
     """ concats args and returns them """
     return str1+str2
