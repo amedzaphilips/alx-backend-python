e6521828c19160e8815f029c4803aa4d4595ff42
amedzaphilips <danielamedzaphilips@gmail.com>
2024-12-12 14:12:32 +0000

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    A program that returns a string rep of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """ gives string rep of a float """
+
+
+    return str(n)
